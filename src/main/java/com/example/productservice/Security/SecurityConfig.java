ee779c291242d5d3d8bed7e23efd10717e75b786
Κωσταντίνος Αχιλλέας Μούσιας <kwstantinosm@hotmail.com>
2025-04-26 11:32:24 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/productservice/Security/SecurityConfig.java
+++ src/main/java/com/example/productservice/Security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
+                                "/swagger-ui/index.html",
                                 "/swagger-ui.html")
                         .permitAll()
                         .anyRequest().authenticated())
