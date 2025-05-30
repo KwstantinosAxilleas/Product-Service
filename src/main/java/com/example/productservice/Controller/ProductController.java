89415add6e57f1ca01c4c91967839e59f14a679b
Κωσταντίνος Αχιλλέας Μούσιας <kwstantinosm@hotmail.com>
2025-05-30 20:09:48 +0300

    Update ProductController.java

--- src/main/java/com/example/productservice/Controller/ProductController.java
+++ src/main/java/com/example/productservice/Controller/ProductController.java
@@ -11,10 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.productservice.Entitys.Product;
 import com.example.productservice.Service.ProductService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
